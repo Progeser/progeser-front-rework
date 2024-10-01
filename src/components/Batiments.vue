4d226fbe9197a99763f0a54aac428a83033dca47
David Lelièvre <david.lelievre@rtssolutions.fr>
2024-10-01 17:16:04 +0200

    update

---
+++ src/components/Batiments.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- Contenu du composant -->
+  </div>
+</template>
+
+<script setup>
+// Ajoute ici ta logique JavaScript (composables, props, etc.)
+</script>
+
+<style scoped>
+/* Ajoute ici les styles spécifiques au composant */
+</style>
