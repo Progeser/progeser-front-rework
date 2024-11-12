e5d5b77186df539d63fa5216eccf043c7098c07d
rlecleire <rlecleire@nextoo.fr>
2024-11-12 09:30:52 +0100

    merge

--- src/pages/buildingPage.vue
+++ src/pages/buildingPage.vue
@@ -10,7 +10,7 @@
     <v-row class="justify-md">
       <div v-for="building in buildingsList" :key="building.id!" class="ma-5">
         <card :title="building.name"
-              @click="navigateToCompartiments(building.id)"
+              @click="navigateToCompartiments(building.id!)"
               img-source="https://img.freepik.com/vecteurs-premium/dessin-batiment-mot-citation-citi-dessine-dessus_951778-115567.jpg"
               :exec="() => navigateToBuildingForm(building.id!)"
         />
@@ -36,7 +36,6 @@ let pageNumber: Ref<number> = ref<number>(1);
 let paginationInformation: Ref<GenericPagination<Building[]> | undefined> = ref<GenericPagination<Building[]> | undefined>();
 const router = useRouter()
 const {t} = useI18n()
-const router = useRouter()
 
 const updateBuildings = async () => {
   paginationInformation.value = await buildingRepository.getBuildingsPage(pageNumber.value);
