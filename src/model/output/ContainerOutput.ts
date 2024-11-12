61ede7ba615e2d5d2649a525348f6f54e0e481a4
Lelievre-david <81243462+Lelievre-david@users.noreply.github.com>
2024-11-12 13:11:05 +0100

    feat(container): listing containers #13

---
+++ src/model/output/ContainerOutput.ts
@@ -0,0 +1,8 @@
+export class ContainerOutput {
+  constructor(
+    public name: string = '',
+    public area: string = '',
+    public dimensions: string | null = null,
+    public shape_name: string = ''
+  ) {}
+}
