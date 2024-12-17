28b7b556ddff4ebaf7374f3f9d4e11ccbc93d52c
David Lelièvre <david72.lelievre@free.fr>
2024-12-17 09:30:11 +0100

    repo + modele

---
+++ src/repository/passwordRepository.ts
@@ -0,0 +1,11 @@
+import FetchService from "@/service/fetchService";
+import {PasswordUpdateOutput} from "@/model/output/PasswordUpdateOutput";
+
+class PasswordRepository {
+
+  private readonly fetchService: FetchService = new FetchService();
+
+  public async resetPassword(passwordUpdate: PasswordUpdateOutput): Promise<void> {
+    return await this.fetchService.put<void, PasswordUpdateOutput>('passwords', passwordUpdate);
+  }
+}
