4ee0cb4584c6c8d185151b0cd4c1f8ab81ccb284
rlecleire <rlecleire@nextoo.fr>
2024-12-19 09:44:47 +0100

    folding menu

--- src/components/MenuSideBarComponent.vue
+++ src/components/MenuSideBarComponent.vue
@@ -7,21 +7,53 @@
       style="background-color: #008B8B; height: 100vh;"
     >
       <v-list>
-        <router-link
-          :to="item.path"
-          v-for="(item, i) in menuRoutes"
-          :key="i"
-          :class="['router-link-hover', { 'active-link': route.path.includes(item.path) }]"
-          style="text-decoration: none;"
-        >
-          <v-list-item>
-            <div class="d-flex align-center">
-              <v-icon class="mr-3 text-white">{{ icons[item.name as string] }}</v-icon>
-              <v-list-item-title class="text-white">{{ t(`menu.name.${String(item.name)}`) }}</v-list-item-title>
-            </div>
-          </v-list-item>
-        </router-link>
+        <template v-for="(item, i) in flattenedRoutes" :key="i">
+          <router-link
+            v-if="!item.children"
+            :to="{ name: item.name }"
+            :class="['router-link-hover', { 'active-link': route.name === item.name }]"
+            style="text-decoration: none;"
+          >
+            <v-list-item>
+              <div class="d-flex align-center">
+                <v-icon class="mr-3 text-white">{{ icons[item.name as string] }}</v-icon>
+                <v-list-item-title class="text-white">{{ t(`menu.name.${item.name as string}`) }}</v-list-item-title>
+              </div>
+            </v-list-item>
+          </router-link>
+
+          <template v-else-if="item.children">
+            <v-list-group v-model="openMenus[item.name as string]" @click="toggleMenu(item.name as string)" base-color="white">
+              <template #activator="{ props }">
+                <v-list-item v-bind="props">
+                  <div class="d-flex align-center w-100">
+                    <v-icon class="mr-3 text-white">{{ icons[item.name as string] }}</v-icon>
+                    <v-list-item-title class="text-white">
+                      {{ t(`menu.name.${item.name as string}`) }}
+                    </v-list-item-title>
+                  </div>
+                </v-list-item>
+              </template>
+
+              <router-link
+                v-for="(child, j) in item.children"
+                :key="j"
+                :to="{ name: child.name }"
+                :class="['router-link-hover', { 'active-link': route.name === child.name }]"
+                style="text-decoration: none;"
+              >
+                <v-list-item class="pl-4">
+                  <div class="d-flex align-center">
+                    <v-icon class="mr-3 text-white">{{ icons[child.name as string] }}</v-icon>
+                    <v-list-item-title class="text-white">{{ t(`menu.name.${child.name as string}`) }}</v-list-item-title>
+                  </div>
+                </v-list-item>
+              </router-link>
+            </v-list-group>
+          </template>
+        </template>
       </v-list>
+
       <div class="user-profile pa-4" ref="profileMenuActivator">
         <v-menu
           v-model="profileMenu"
@@ -39,6 +71,7 @@
                 <div class="text-white font-weight-medium">{{ `${user?.first_name} ${user?.last_name}` }}</div>
                 <div class="text-white text-caption">{{ user?.email }}</div>
               </div>
+              <v-icon class="ml-auto text-white">mdi-chevron-down</v-icon>
             </div>
           </template>
           <v-list>
@@ -52,6 +85,7 @@
         </v-menu>
       </div>
     </v-navigation-drawer>
+
     <v-app-bar app color="white">
       <v-row align="center" justify="start">
         <v-col class="d-flex" cols="auto">
@@ -63,6 +97,7 @@
         <NavigationStepper class="ml-2"/>
       </v-row>
     </v-app-bar>
+
     <v-main class="bg-grey-lighten-2">
       <slot/>
     </v-main>
@@ -70,39 +105,63 @@
 </template>
 
 <script setup lang="ts">
-import {onBeforeMount, ref} from 'vue';
-import menuRoutes, {icons} from "@/router/menuRoutes";
-import {useRoute} from "vue-router";
-import {useI18n} from "vue-i18n";
-import {UserModel} from "@/model/UserModel";
-import {useUserStore} from "@/store/UserStore";
+import { onBeforeMount, ref, computed, reactive } from 'vue';
+import menuRoutes, { icons } from "@/router/menuRoutes";
+import { useRoute } from "vue-router";
+import { useI18n } from "vue-i18n";
+import { UserModel } from "@/model/UserModel";
+import { useUserStore } from "@/store/UserStore";
 import NavigationStepper from "@/components/NavigationStepper.vue";
 import router from "@/router";
-import {useAuthStore} from "@/store/AuthStore";
+import { useAuthStore } from "@/store/AuthStore";
 
 const drawer = ref(false);
 const route = useRoute();
-const {t} = useI18n();
+const { t } = useI18n();
 const userStore = useUserStore();
 const user = ref<UserModel | null>(null);
 const AuthStore = useAuthStore();
 const profileMenu = ref(false);
 
-onBeforeMount(async () => {
-    user.value = await userStore.getCurrentUser()
-  }
-)
+const openMenus = reactive<Record<string, boolean>>({});
 
-const redirectToProfilePage =  () => {
+const toggleMenu = (menuName: string) => {
+  openMenus[menuName] = !openMenus[menuName];
+};
+
+const flattenedRoutes = computed(() => {
+  return menuRoutes.map(route => {
+    if (route.children) {
+      // Initialize menu state for routes with children
+      if (!(route.name as string in openMenus)) {
+        openMenus[route.name as string] = false;
+      }
+      return {
+        ...route,
+        children: route.children.map(child => ({
+          ...child,
+          path: `${route.path}${child.path}`
+        }))
+      };
+    }
+    return route;
+  });
+});
+
+onBeforeMount(async () => {
+  user.value = await userStore.getCurrentUser()
+});
+
+const redirectToProfilePage = () => {
   profileMenu.value = false;
-  router.push({name: 'Profile'});
-}
+  router.push({ name: 'Profile' });
+};
 
 const logout = () => {
   profileMenu.value = false;
   AuthStore.logout();
-  router.push({name: "Login"});
-}
+  router.push({ name: "Login" });
+};
 </script>
 
 <style scoped>
@@ -140,5 +199,9 @@ const logout = () => {
   background-color: #00B0B0;
   transition: background-color 0.3s ease;
   border-radius: 4px;
+}
+
+:deep(.v-list-group__items) {
+  background-color: rgba(0, 0, 0, 0.1);
 }
 </style>
