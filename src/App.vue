4144808f5140da9248b96811224308774aaa6ddd
rlecleire <rlecleire@nextoo.fr>
2024-12-19 16:04:15 +0100

    rm console log

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app>
+    <v-snackbar
+      v-model="snackbarStore.show"
+      :color="snackbarStore.color"
+      :timeout="snackbarStore.timeout"
+      location="top"
+    >
+      {{ snackbarStore.message }}
+    </v-snackbar>
     <v-main>
       <router-view v-if="isLoginRoute"/>
       <MenuSideBar v-else>
@@ -15,10 +23,13 @@
 import MenuSideBar from "@/components/MenuSideBarComponent.vue";
 import router from "@/router"
 import {computed} from "vue";
+import {useSnackbarStore} from "@/store/snackbarStore";
 
 const isLoginRoute = computed(() => {
   const currentRouteName = router.currentRoute.value.name;
   return currentRouteName === 'Login' || currentRouteName === 'Request' || currentRouteName === 'CatchAll' || router.currentRoute.value.path === '/';
 });
+
+const snackbarStore = useSnackbarStore()
 
 </script>
